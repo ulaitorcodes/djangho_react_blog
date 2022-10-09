c199baffd1ca66df50bccd46fd906013dcd71cd7
buffetng <ulaitorcodes@gmail.com>
2022-10-09 17:07:15 +0100

    added react

--- backend/settings.py
+++ backend/settings.py
@@ -38,9 +38,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "todo",
+    "corsheaders",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -48,6 +51,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ORIGIN_WHIOTELIST = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = "backend.urls"
